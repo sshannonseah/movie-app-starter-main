6320322b9395c07af3c587ba45a14ca281fbeac0
sshannonseah <23018646@myrp.edu.sg>
2025-01-08 16:06:56 +0800

    first commit

---
+++ src/App.js
@@ -0,0 +1,49 @@
+import { useEffect, useState } from "react";
+const KEY = "951dc136";
+
+function App() {
+  const [movies, setMovies] = useState([]);
+  const [query , setQuery] = useState('batman');
+
+
+  useEffect (() => {
+    const controller = new AbortController();
+    fetch(`https://www.omdbapi.com/?apikey=${KEY}&s=${query}`, {signal: controller.signal})
+      .then((res) => res.json())
+      .then((data) => data.Response ==="True" && setMovies(data.Search))
+      .catch((err) => console.log(err));
+      return () => {
+        controller.abort();
+      };
+  },[query]);
+
+
+  return (
+    <div>
+      <h1>Movies</h1>
+      <input 
+      type="text"
+      placeholder="Search movies..."
+      value={query} 
+      onChange={(e) => setQuery(e.target.value)} />
+      <table>
+        <thead>
+          <tr>
+            <th>Title</th>
+            <th>Year</th>
+          </tr>
+        </thead>
+        <tbody>
+          {movies.map((movie) => (
+            <tr key={movie.imdbID}>
+              <td>{movie.Title}</td>
+              <td>{movie.Year}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+}
+
+export default App;
